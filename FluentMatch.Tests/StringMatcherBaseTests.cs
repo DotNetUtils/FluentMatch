656f0a37846ef77aec3b512cce0136f01d1ee876
BJ Myers <bjmyers17@gmail.com>
2020-05-03 11:50:30 -0600

    Added fluent methods for concatenating string matchers using And & Or logic.

---
+++ FluentMatch.Tests/StringMatcherBaseTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace DotNetUtils.FluentMatch.Tests
+{
+    public sealed class StringMatcherBaseTests
+    {
+        private sealed class AnyStringMatcher : StringMatcher
+        {
+            public override bool Matches(string input) => true;
+        }
+
+        [Fact]
+        public void And_ThrowsNullArgumentException()
+        {
+            static object callAndWithNull() => new AnyStringMatcher().And(null);
+            Assert.Throws<ArgumentNullException>(callAndWithNull);
+        }
+
+        [Fact]
+        public void Or_ThrowsNullArgumentException()
+        {
+            static object callOrWithNull() => new AnyStringMatcher().Or(null);
+            Assert.Throws<ArgumentNullException>(callOrWithNull);
+        }
+    }
+}
--- FluentMatch/StringMatcher.cs
+++ FluentMatch/StringMatcher.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNetUtils.FluentMatch
+﻿using System;
+
+namespace DotNetUtils.FluentMatch
 {
     /// <summary>
     /// Provides methods for string matching based on implementation-specific logic.
@@ -12,5 +14,35 @@
         /// <param name="input">The <see cref="string" /> input to test.</param>
         /// <returns><c>true</c> if this instance matches the specified input string; otherwise, <c>false</c>.</returns>
         public abstract bool Matches(string input);
+
+        /// <summary>
+        /// Adds an additional <see cref="StringMatcher" /> that must also be satisfied for an input to match.
+        /// </summary>
+        /// <param name="additionalStringMatcher">The additional <see cref="StringMatcher" /> object.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="additionalStringMatcher" /> is null.</exception>
+        public StringMatcher And(StringMatcher additionalStringMatcher)
+        {
+            if (additionalStringMatcher is null)
+            {
+                throw new ArgumentNullException(nameof(additionalStringMatcher));
+            }
+
+            return new AndStringMatcher(this, additionalStringMatcher);
+        }
+
+        /// <summary>
+        /// Adds an alternate <see cref="StringMatcher" /> that can be satisfied for an input to match.
+        /// </summary>
+        /// <param name="alternateStringMatcher">The alternate <see cref="StringMatcher" /> object.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="alternateStringMatcher" /> is null.</exception>
+        public StringMatcher Or(StringMatcher alternateStringMatcher)
+        {
+            if (alternateStringMatcher is null)
+            {
+                throw new ArgumentNullException(nameof(alternateStringMatcher));
+            }
+
+            return new OrStringMatcher(this, alternateStringMatcher);
+        }
     }
 }
